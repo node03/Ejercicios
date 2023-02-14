f02d3a2c3e5ed1d5eea87dcd85b24dd2fd276f00
node03 <renato@deltait.es>
2023-02-14 21:47:10 +0100

    ejercicio 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(1,5,9);
+        System.out.println(resultado);
+
+    }
+
+public static int suma (int a, int b, int c) {
+        return a + b + c;
+}
+}
